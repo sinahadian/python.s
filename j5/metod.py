821a94598cf73b39970d256761e97d0da2478a69
sinahadian <140071600+sinahadian@users.noreply.github.com>
2023-08-08 15:14:53 +0330

    Add files via upload

---
+++ j5/metod.py
@@ -0,0 +1,9 @@
+# a = 'class python'
+# a = a.center(60.'0')
+# print (a)
+# name = 'my name is sina'
+# name.encode
+# text = 'my name is stale
+# text = encode
+b ='s/i/n/a/'
+b = b.expandtabs
---
+++ j5/name.py
@@ -0,0 +1,5 @@
+name_family = str(input('enter your name :'))
+if name_family == int or float:
+    print('yor name is fals')
+    name_family = name_family.capitalize
+    print('your name and family is :',name_family)
